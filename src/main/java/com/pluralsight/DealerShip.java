7c1aa2560e0865d693f3197ec4678cdf704b80ad
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-08 09:59:06 -0400

    DealerShip , Vehicle class created.

--- src/main/java/com/pluralsight/DealerShip.java
+++ src/main/java/com/pluralsight/DealerShip.java
@@ -1,4 +1,75 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class DealerShip {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public DealerShip(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double mac){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getVehiclesByType(String type){
+        return null;
+    }
+
+    // TODO: Create the method
+    public ArrayList<Vehicle> getAllVehicle(){
+        return this.inventory;
+    }
+
+    // DONE: Create the method
+    public void addVehicle(Vehicle vehicle){
+        this.inventory.add(vehicle);
+    }
+
+    // TODO: Create the method
+    public void removeVehicle(Vehicle vehicle){
+    }
+
+
+    // region getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    // endregion
 }
