69503f116502ad79b0d5c3a731ac60f1a8f33439
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-11 13:00:03 -0400

    Change FilePath modifier

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -4,13 +4,13 @@ import java.io.*;
 
 public class DealershipFileManager {
 
-    public static final String FILE_LOCATION = "src/main/resources/inventory.csv";
+    private static final String FILE_PATH = "src/main/resources/inventory.csv";
 
     public static Dealership getDealership() {
         Dealership dealership;
         Vehicle vehicle;
         try {
-            FileReader fileReader = new FileReader(FILE_LOCATION);
+            FileReader fileReader = new FileReader(FILE_PATH);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String currentLine = bufferedReader.readLine();
             dealership = createDealershipObject(currentLine);
@@ -20,29 +20,17 @@ public class DealershipFileManager {
             }
             return dealership;
         } catch (IOException e) {
-            System.err.println("Wrong file location: " + FILE_LOCATION + " " + e);
+            System.err.println("Wrong file location: " + FILE_PATH + " " + e);
         }
         return null;
 
     }
 
-    private static Vehicle createVehicle(String currentLine) {
-        String[] lineSpilt = currentLine.split("\\|");
-        int vin = Integer.parseInt(lineSpilt[0]);
-        int year = Integer.parseInt(lineSpilt[1]);
-        String make = lineSpilt[2];
-        String model = lineSpilt[3];
-        String type = lineSpilt[4];
-        String color = lineSpilt[5];
-        int odometer = Integer.parseInt(lineSpilt[6]);
-        double price = Double.parseDouble(lineSpilt[7]);
-        return new Vehicle(vin, year, make, model, type, color, odometer, price);
-    }
-
     //DONE : Create the method
+
     public static void saveDealership(Dealership dealerShip) {
         try {
-            FileWriter fileWriter = new FileWriter(FILE_LOCATION);
+            FileWriter fileWriter = new FileWriter(FILE_PATH);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write(dealerShip.getName() + "|" + dealerShip.getAddress() + "|" + dealerShip.getPhone());
             for (Vehicle vehicle : dealerShip.getAllVehicle()) {
@@ -57,7 +45,6 @@ public class DealershipFileManager {
 
 
     }
-
     private static Dealership createDealershipObject(String line) {
         String[] nameAddressNumber = line.split("\\|");
         String name = nameAddressNumber[0];
@@ -66,5 +53,18 @@ public class DealershipFileManager {
 
         return new Dealership(name, address, phoneNumber);
 
+    }
+
+    private static Vehicle createVehicle(String currentLine) {
+        String[] lineSpilt = currentLine.split("\\|");
+        int vin = Integer.parseInt(lineSpilt[0]);
+        int year = Integer.parseInt(lineSpilt[1]);
+        String make = lineSpilt[2];
+        String model = lineSpilt[3];
+        String type = lineSpilt[4];
+        String color = lineSpilt[5];
+        int odometer = Integer.parseInt(lineSpilt[6]);
+        double price = Double.parseDouble(lineSpilt[7]);
+        return new Vehicle(vin, year, make, model, type, color, odometer, price);
     }
 }
